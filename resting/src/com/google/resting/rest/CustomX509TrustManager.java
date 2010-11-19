1f7ddb5b6d333ecec444ff8ca649aa48c80487a5
sujata.de <sujata.de@17e3c768-ce7c-96e8-a128-80a8a6505a7e>
2010-11-19 12:17:29 +0000

    Package structure and third party libraries.

---
+++ resting/src/com/google/resting/rest/CustomX509TrustManager.java
@@ -0,0 +1,40 @@
+package com.google.resting.rest;
+
+
+
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.X509TrustManager;
+/**
+ * Custom Trust Manager. Esp. suitable for Android.
+ * 
+ * @author sujata.de
+ * @since resting 0.1
+ *
+ */
+
+public class CustomX509TrustManager implements X509TrustManager {
+
+	/**
+	 * @see javax.net.ssl.X509TrustManager#checkClientTrusted(X509Certificate[],String authType)
+	 */
+	public void checkClientTrusted(X509Certificate[] certificates, String authType) throws CertificateException {
+		
+	}
+
+	/**
+	 * @see javax.net.ssl.X509TrustManager#checkServerTrusted(X509Certificate[],String authType)
+	 */
+	public void checkServerTrusted(X509Certificate[] certificates, String authType) throws CertificateException {
+
+	}
+
+	/**
+	 * @see javax.net.ssl.X509TrustManager#getAcceptedIssuers()
+	 */
+	public X509Certificate[] getAcceptedIssuers() {
+		return null;
+	}
+
+}

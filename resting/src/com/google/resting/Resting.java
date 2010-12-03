619f783bc63dc9e34c2b4f958332f9825d31a0f5
sujata.de <sujata.de@17e3c768-ce7c-96e8-a128-80a8a6505a7e>
2010-12-03 07:40:41 +0000

    Updated with license information

--- resting/src/com/google/resting/Resting.java
+++ resting/src/com/google/resting/Resting.java
@@ -1,3 +1,19 @@
+ /*
+ * Copyright (C) 2010 Google Code.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.resting;
 
 import java.util.List;
@@ -5,10 +21,8 @@ import java.util.List;
 import com.google.resting.component.OperationType;
 import com.google.resting.component.RequestParams;
 import com.google.resting.component.impl.Alias;
-import com.google.resting.component.impl.BasicRequestParams;
 import com.google.resting.component.impl.ServiceResponse;
 import com.google.resting.helper.RestingHelper;
-import com.google.resting.test.Product;
 import com.google.resting.transform.TransformationType;
 /**
  * This is the main class for using resting. 
--- resting/src/com/google/resting/test/Test.java
+++ resting/src/com/google/resting/test/Test.java
@@ -1,3 +1,19 @@
+ /*
+ * Copyright (C) 2010 Google Code.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.resting.test;
 
 import java.util.List;
@@ -33,13 +49,18 @@ public class Test {
 */		
 		//List<ResultSet> resultset=Resting.getByXML("http://local.yahooapis.com/MapsService/V1/geocode?appid=YD-9G7bey8_JXxQP6rxl.fBFGgCdNjoDMACQA--&state=CA", ResultSet.class, alias);
 		//System.out.println(resultset.toString());
-		RequestParams params = new BasicRequestParams();
-		params.add("facets", "[\"brandNameFacet\"]");
-		params.add("key", "<your key>");
+//		RequestParams params = new BasicRequestParams();
+//		params.add("facets", "[\"brandNameFacet\"]");
+//		params.add("key", "ff25d179c8b2b8c4f40c78a37b458fd0415ccb6d");
 		
 		//ServiceResponse serviceResponse=RestingHelper.execute("http://api.zappos.com/Search",params,OperationType.GET);
-		List<Facets> facets=Resting.getByJSON("http://api.zappos.com/Search",params, Facets.class, "facets");
-		System.out.println(facets.get(0).getValues().size());
+		//List<Facets> facets=Resting.getByJSON("http://api.zappos.com/Search",params, Facets.class, "facets");
+		//System.out.println(facets.get(0).getValues().size());
+		RequestParams params = new BasicRequestParams();
+		params.add("key", "ff25d179c8b2b8c4f40c78a37b458fd0415ccb6d");
+		List<Product> products=Resting.getByJSON("http://api.zappos.com/Product/7564933,7590514",80,params, Product.class, "product");
+		System.out.println(products.get(0).getProductId());
+
 	}
 
 }//Test 

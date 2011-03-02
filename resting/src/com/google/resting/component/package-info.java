aff6a5e1d260668ea3633e42d395accba9a0bfac
sujata.de <sujata.de@17e3c768-ce7c-96e8-a128-80a8a6505a7e>
2011-03-02 13:31:48 +0000

    Added package info for javadoc

---
+++ resting/src/com/google/resting/component/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides {@link com.google.resting.component.RequestParams} which can be extended to handle customized collection of query 
+ * parameters for REST invocation. The directly known subclasses available are {@link com.google.resting.component.BasicRequestParams}
+ * and {@link com.google.resting.component.impl.json.JSONRequestParams}.   
+ */
+package com.google.resting.component;
---
+++ resting/src/com/google/resting/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package provides the {@link com.google.resting.Resting} class to invoke rest services and transform the HTTP response into value objects.
+ *
+ * <p>The primary class to use is {@link com.google.resting.Resting}, where the main APIs will be get(), put(), post(), delete() for 
+ * rest service invocation, getByJSON() and getByXML() to invoke the rest services and convert the response (JSON or XML) into value objects.
+ * 
+ * </p>  
+ *
+ * @author sujata.de
+ */
+package com.google.resting;

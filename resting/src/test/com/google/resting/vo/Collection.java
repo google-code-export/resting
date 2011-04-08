5c86dc59c237aa40750d59b98ec0134b67a356d4
sujata.de <sujata.de@17e3c768-ce7c-96e8-a128-80a8a6505a7e>
2011-04-08 15:00:49 +0000

    Adding implicit collections for XML alias

---
+++ resting/src/test/com/google/resting/vo/Collection.java
@@ -0,0 +1,28 @@
+package test.com.google.resting.vo;
+
+public class Collection {
+	
+	private String name;
+	private boolean enabled;
+	private boolean defaultSearch;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public boolean isDefaultSearch() {
+		return defaultSearch;
+	}
+	public void setDefaultSearch(boolean defaultSearch) {
+		this.defaultSearch = defaultSearch;
+	}
+	
+
+}
---
+++ resting/src/test/com/google/resting/vo/Collections.java
@@ -0,0 +1,17 @@
+package test.com.google.resting.vo;
+
+import java.util.List;
+
+public class Collections {
+	
+	private List<Collection> entries;
+
+	public List<Collection> getEntries() {
+		return entries;
+	}
+
+	public void setEntries(List<Collection> entries) {
+		this.entries = entries;
+	}
+
+}

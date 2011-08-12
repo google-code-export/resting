6da9031980e31de20437964b06bb7d91a88cf580
sujata.de <sujata.de@17e3c768-ce7c-96e8-a128-80a8a6505a7e>
2011-08-12 07:56:32 +0000

    Incorporated different types of charset

--- resting/src/com/google/resting/component/content/contentdecorator/ByteContentData.java
+++ resting/src/com/google/resting/component/content/contentdecorator/ByteContentData.java
@@ -1,3 +1,18 @@
+ /*
+ * Copyright (C) 2011 Google Code.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.resting.component.content.contentdecorator;
 
 import java.io.UnsupportedEncodingException;
@@ -5,6 +20,13 @@ import java.io.UnsupportedEncodingException;
 import com.google.resting.component.EncodingTypes;
 import com.google.resting.component.content.IContentData;
 
+/**
+ * Byte content data from REST response
+ * 
+ * @author sujata.de
+ * @since resting0.6
+ *
+ */
 public class ByteContentData implements IContentData {
 	
 	private byte[] content=null;
--- resting/src/com/google/resting/component/content/contentdecorator/StringContentData.java
+++ resting/src/com/google/resting/component/content/contentdecorator/StringContentData.java
@@ -1,12 +1,31 @@
+ /*
+ * Copyright (C) 2011 Google Code.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.resting.component.content.contentdecorator;
 
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.google.resting.component.EncodingTypes;
-import com.google.resting.component.content.ContentType;
 import com.google.resting.component.content.IContentData;
+
+/**
+ * String content data from REST response. 
+ * 
+ * @author sujata.de
+ * @since resting 0.6
+ */
 
 public class StringContentData implements IContentData {
 	

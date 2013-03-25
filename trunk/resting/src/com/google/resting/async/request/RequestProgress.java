26164950512379d3248318aef2f652dea6f38ecb
sujata.de@gmail.com <sujata.de@gmail.com@17e3c768-ce7c-96e8-a128-80a8a6505a7e>
2013-03-25 15:45:32 +0000

    Making JSONTransformer more robust

---
+++ trunk/resting/src/com/google/resting/async/request/RequestProgress.java
@@ -0,0 +1,36 @@
+package com.google.resting.async.request;
+
+/**
+ * Model class for getting progress
+ *
+ */
+public class RequestProgress {
+
+    private RequestStatus status;
+    private float progress;
+
+    public RequestProgress(final RequestStatus status) {
+        this(status, 0);
+    }
+
+    public RequestProgress(final RequestStatus status, final float progress) {
+        this.status = status;
+        this.progress = progress;
+    }
+
+    public RequestStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(final RequestStatus status) {
+        this.status = status;
+    }
+
+    public float getProgress() {
+        return progress;
+    }
+
+    public void setProgress(final float progress) {
+        this.progress = progress;
+    }
+}
---
+++ trunk/resting/src/test/com/google/resting/vo/Err.java
@@ -0,0 +1,11 @@
+package test.com.google.resting.vo;
+
+public class Err {
+	
+	private String err;
+	
+	public String toString(){
+		return err;
+	}
+	
+}
